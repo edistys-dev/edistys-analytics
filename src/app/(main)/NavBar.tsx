765ffd21662b455891657247780b3479809c795d
reasat-rafio <rafioar96@gmail.com>
2024-11-15 17:09:17 +0600

    branding added

--- src/app/(main)/NavBar.tsx
+++ src/app/(main)/NavBar.tsx
@@ -84,7 +84,7 @@ export function NavBar() {
         <Icon size="lg">
           <Icons.Logo />
         </Icon>
-        <Text>umami</Text>
+        <Text>edistys</Text>
       </div>
       <div className={styles.links}>
         {links.map(({ url, label }) => {
